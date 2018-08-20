282d4d34ad40be0e9161a1b42bbb5ed2e368c997
Bloom <skaywoker6@gmail.com>
2018-08-20 14:15:29 +0300

    Add Crud repo for User entity

--- src/main/java/com/bloom/mvcspring/tut/Repos/UserRepository.java
+++ src/main/java/com/bloom/mvcspring/tut/Repos/UserRepository.java
@@ -1,4 +1,10 @@
 package com.bloom.mvcspring.tut.Repos;
 
-public interface UserRepository {
+import com.bloom.mvcspring.tut.DAO.User;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+// репозиторій дозволяє отримати повний список полів або отримати їх по іднетифікатору
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
